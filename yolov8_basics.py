dfdf8ce86178bb050a1a27496b0c2217960db411
jeffrey ching <jching26@yahoo.com>
2023-07-30 14:57:46 -0700

    test

--- yolov8_basics.py
+++ yolov8_basics.py
@@ -5,10 +5,14 @@ import numpy
 model = YOLO("yolov8n.pt", "v8")  
 
 # predict on an image
-detection_output = model.predict(source="inference/images/img0.JPG", conf=0.25, save=True) 
+#detection_output = model.predict(source="inference/images/armor1.JPG", conf=0.25, save=True, show=True) 
+detection_output = model.predict(source="1", conf=0.25, save=True, show=True) 
+
+#show = true shows the camera GUI when code is executed
 
 # Display tensor array
 print(detection_output)
 
 # Display numpy array
 print(detection_output[0].numpy())
+
